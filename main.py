656c0da7d32d45f540eaa012c4fcd21cfe5fb77f
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-16 10:47:35 +0530

    created a function to return single patient using path param

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Run -> $ uvicorn main:app --reload
 
-from fastapi import FastAPI 
-from load_patients_data import load_patients_data
+from fastapi import FastAPI, Path, HTTPException
+from utils import load_patients_data
 
 app = FastAPI()
 
@@ -15,8 +15,15 @@ async def hello_world():
 async def about():
     return {"message":"Fully functional API to manage patients records"}
 
-@app.get("/view")
-async def view():
+@app.get("/patients")
+async def patients():
     global patients_data
     patients_data = await load_patients_data()
     return patients_data
+
+@app.get("/patients/{patient_id}")
+async def get_one_patient(patient_id:str = Path(..., description='ID of the patient in the DB', example='P001')):
+    if patient_id in patients_data:
+        return {'patient_id':patient_id,"patient_details":patients_data[patient_id]}
+    
+    raise HTTPException(status_code=404, detail="Patient not found")
