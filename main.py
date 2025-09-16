f92b860f66629aeda30ac7418900024f11a0631c
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-16 11:02:26 +0530

    added a function for query params to retrieve patients data by ordering them by height, weight or bmi

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Run -> $ uvicorn main:app --reload
 
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 from utils import load_patients_data
 
 app = FastAPI()
@@ -21,9 +21,29 @@ async def patients():
     patients_data = await load_patients_data()
     return patients_data
 
+@app.get("/patients/sort")
+async def sort_patients(
+    sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), 
+    order = Query('asc', description='Sort asc or desc order')
+    ):
+
+    valid_fields = ['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field. Select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400, detail=f'Invalid order. Select between \'asc\' or \'desc\'')
+    
+    sort_order = True if order == 'desc' else False
+    sorted_patients_data = sorted(patients_data.values(), key=lambda x:x.get(sort_by,0), reverse=sort_order)
+    
+    return sorted_patients_data
+
 @app.get("/patients/{patient_id}")
 async def get_one_patient(patient_id:str = Path(..., description='ID of the patient in the DB', example='P001')):
+    
     if patient_id in patients_data:
         return {'patient_id':patient_id,"patient_details":patients_data[patient_id]}
     
     raise HTTPException(status_code=404, detail="Patient not found")
+
