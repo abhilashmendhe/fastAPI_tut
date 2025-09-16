6508a9ad92b6263d8fe9258f78d14714de3785ea
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-16 12:10:13 +0530

    worked with Annoated and Field validators

--- pydantic_tut/test1.py
+++ pydantic_tut/test1.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel,EmailStr,AnyUrl
-from typing import List, Dict, Optional
+from pydantic import BaseModel,EmailStr,AnyUrl,Field
+from typing import List, Dict, Optional, Annotated
 
 class PatientContactDetails(BaseModel):
     email: EmailStr 
@@ -7,12 +7,13 @@ class PatientContactDetails(BaseModel):
     
 class Patient(BaseModel):
     
-    name: str 
-    age: int 
-    weight: float
-    married: bool = False
+    # name: str = Field(max_length=50)
+    name: Annotated[str, Field(max_length=50, title="Name of the patient", description="Give name of the patient in less than 50 characters", examples=['Abhilash', 'Nitish'])]
+    age: int = Field(gt=0,lt=120)
+    weight: float = Field(gt=0)
+    married: Annotated[bool, Field(default=None, description='Is the patient married or not?')]
     linkedin: AnyUrl
-    allergies: Optional[List[str]]= None
+    allergies: Optional[List[str]] = Field(max_length=5)
     # contact_details: Dict[str,str]
     contact_details: PatientContactDetails
     
