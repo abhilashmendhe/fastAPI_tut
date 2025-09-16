ea56509366f47e3c476225fadcb1912d20331342
abhilashmendhe <abhilashmendhe@gmail.com>
2025-09-16 15:51:53 +0530

    added a save function that saves the data into json file

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 import json
 import aiofiles
 
-async def load_patients_data(path="./data/patients.json"):
+def load_patients_data(path="./data/patients.json"):
+    
+    with open(path, 'r') as f:
+        data = f.read()
+
+    return json.loads(data)
+
+async def async_load_patients_data(path="./data/patients.json"):
     
     async with aiofiles.open(path, 'r') as f:
         data = await f.read()
 
     return json.loads(data)
+
+async def save_data(data, path="./data/patients.json"):
+    with open(path, 'w') as f:
+        json.dump(data, f)
